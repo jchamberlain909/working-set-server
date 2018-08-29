668a8321add0032f6e35b42509823600ccf1524b
jchamberlain909 <jchamberlain909@gmail.com>
2018-08-29 14:31:21 -0400

    added project routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   resources :users, only: [:create, :index, :show, :update, :destroy]
   resources :companies, only: [:create, :index, :show, :update, :destroy]
 
-  
+  get '/companies/:company_id/projects', to: 'projects#index'
+  get '/companies/:company_id/projects/:project_id', to: 'projects#show'
+  post '/companies/:company_id/projects', to: 'projects#create'
+  patch 'companies/:company_id/projects/:project_id', to: 'projects#update'
+  delete 'companies/:company_id/projects/:project_id', to: 'projects#destroy'
 
 end
